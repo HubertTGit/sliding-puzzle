c4a7dc778595296fd54c390f0806d0921b5a151f
HuberttGit <hubert.talib@gmail.com>
2024-02-09 16:12:15 +0100

    color update

--- src/Card.tsx
+++ src/Card.tsx
@@ -15,7 +15,9 @@ export default function Card({ card, onCardClick, index }: CardProps) {
       }}
       className={`w-24 h-24 flex justify-center items-center border rounded-sm  ${
         card.isEmpty && 'bg-transparent'
-      } ${card.originalPosition === index && 'bg-green-300'}`}
+      } ${card.originalPosition === index && 'bg-green-300'} ${
+        card.isEnabled && 'transition-colors hover:bg-green-400'
+      }`}
     >
       {card.isEmpty ? '' : card.originalPosition + 1}
     </div>
